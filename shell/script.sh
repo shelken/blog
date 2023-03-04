7c636c3e0a186a14c6efdce84161187a37f99dac
shelken <shelken.pxk@gmail.com>
2023-03-05 05:38:53 +0800

    update

---
+++ shell/script.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env zsh
+
+python3 asciinema_font_subsetter.py --input-font /Users/shelken/Library/Fonts/MesloLGS\ NF\ Regular.ttf --output ../assets/fonts/glyphs.ttf --casts ../content --exclude-ascii
